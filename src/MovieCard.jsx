811ed5128d2f5f67bc6030708ea9d5ad83e7bbcf
akash kennedy <akashkennedy1@gmail.com>
2024-03-30 19:20:22 +0530

    Created styles

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,9 +4,6 @@ const MovieCard = ({ movie }) => {
   return (
     <div className="movie">
       <div>
-        <p>{movie.Year}</p>
-      </div>
-      <div>
         <img
           src={
             movie.Poster !== "N/A"
@@ -17,6 +14,9 @@ const MovieCard = ({ movie }) => {
         />
       </div>
       <div>
+      <div>
+        <p>{movie.Year}</p>
+      </div>
         <span>{movie.Type}</span>
         <h3>{movie.Title}</h3>
       </div>
